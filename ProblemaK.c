922799f2257974c30d6a5e571c09ceba34da31ac
FranciscoMonteiro694 <34212758+FranciscoMonteiro694@users.noreply.github.com>
2019-05-14 14:59:11 +0100

    Update ProblemaK.c

--- ProblemaK.c
+++ ProblemaK.c
@@ -13,14 +13,27 @@
 0
 0
 */
+int acumulador, tempo = 0;
+int indexGlobal = 0;
+void dfsAlgorithm(int **grafo, int *visited, int *dfs, int nPlaces, int index);
 void analisa(int nPlaces);
 void imprimeGrafos(int **grafo,int n);
+void art(int v,int *dfs, int *low, int **grafo,int nPlaces,int *parent);
+int min(int a,int b);
+
 int main(int argc, const char * argv[]) {
+    acumulador=0;
     int nPlaces;
+    char*temp = (char*)malloc(sizeof(char)*50);
     while(1){/* Usar breaks aqui à campeão */
-        scanf("%d\n",&nPlaces);
-        if(nPlaces==0)
+        /*printf("ou seja, estou aqui certo?\n");*/
+        gets(temp);
+        /*printf("e deu isto: %d !\n", nPlaces);*/
+        if(atoi(temp)==0) {
+            /*printf("entrei aqui portanto sai");*/
             break;
+        }
+        nPlaces = atoi(temp);
         analisa(nPlaces);
     }
 
@@ -33,19 +46,99 @@ void analisa(int nPlaces){
     char *buffer;
     char *token;
     grafo=(int**)malloc(sizeof(int*)*nPlaces);
-    buffer=(char*)malloc(sizeof(char)*50);
+    buffer=(char*)malloc(sizeof(char)*200);
+
     for (i=0;i<nPlaces;i++){
         grafo[i]=(int*)malloc(sizeof(int)*nPlaces);
         memset (grafo[i], 0, sizeof (int) * nPlaces);
     }
-    while(fgets(buffer, sizeof(buffer), stdin)){
-//        if (strcmp(buffer,"0")==0)
-//            printf("Fodasse");
-        printf("valor do buffer %s",buffer);
+    
+    int *visited = (int*)malloc(sizeof(int)*nPlaces);
+    memset(visited, 0, sizeof(int)*nPlaces);
+    int *dfs = (int*)malloc(sizeof(int)*nPlaces);
+    int *low = (int*)malloc(sizeof(int)*nPlaces);
+    int *parent = (int*)malloc(sizeof(int)*nPlaces);
+    
+
+    for (i=0;i<nPlaces;i++){
+        low[i] = nPlaces+1;
+        dfs[i] = -1;
+        parent[i] = -1;
+    }
+    
+    while(gets(buffer)){
+        if (strcmp(buffer,"0")==0){
+            /*printf("Fodasse\n");*/
+            break;
+        }
+        
+        //printf("valor do buffer %s\n",buffer);
         token=strtok(buffer," ");
-        while(token != NULL){
-            //printf("Valor do token %s",token);
+        i = atoi(token); /*inicial*/
+        while(1){
+            /*printf("Valor do token %s\n",token);*/
             token = strtok(NULL," ");
+            if(token != NULL) {
+                grafo[i-1][atoi(token)-1] = 1; /*criar a ligacao*/
+                grafo[atoi(token)-1][i-1] = 1; /*criar a ligacao (bidirecional)*/
+            } else
+                break;
+        }
+        fflush(stdin);
+        /*imprimeGrafos(grafo, nPlaces);*/
+    }
+    
+    /*dfsAlgorithm(grafo, visited, dfs, nPlaces, 0);*/
+    indexGlobal=0;
+    for(i=0;i<nPlaces;i++){
+        art(i,dfs,low,grafo,nPlaces,parent);
+        tempo=0;
+    }
+    /*printf("Valor do acumulador %d\n",acumulador);*/
+    acumulador = 0;
+}
+
+void art(int v,int *dfs, int *low, int **grafo,int nPlaces,int *parent){
+    
+    int j;
+    int children;
+    children=0;
+    low[v]=dfs[v]=++tempo;
+    /* Percorre o grafo */
+    for(j=0;j<nPlaces;j++){
+        if(grafo[v][j]==1){
+            if(dfs[j]==-1){
+                children++;
+                parent[v]=j;
+                art(j,dfs,low,grafo,nPlaces,parent);
+                low[v]=min(low[v],low[j]);
+                if(dfs[v]==1 && dfs[j]!=2)
+                    acumulador++;
+                if(dfs[v]!=1 && low[j]>=dfs[v])
+                    acumulador++;
+            }
+            else if(j!=parent[v]){
+                low[v]=min(low[v],dfs[j]);
+            }
+        }
+    }
+}
+
+int min(int a,int b){
+    if (a>b)
+        return b;
+    else
+        return a;
+}
+
+void dfsAlgorithm(int**grafo, int*visited, int*dfs, int nPlaces, int index){
+    int j;
+    dfs[indexGlobal++] = index+1;
+    visited[index] = 1;
+    
+    for(j= 0; j<nPlaces; j++){
+        if(!visited[j] && grafo[index][j] == 1) {
+            dfsAlgorithm(grafo, visited, dfs, nPlaces, j);
         }
     }
 }
